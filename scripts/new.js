f3c386ad252058c9ec62c5267cec9fcf9ec34aaa
Saurav Pratihar <pratihar.saurav@gmail.com>
2017-04-24 22:51:13 +0530

    errors :(

---
+++ scripts/new.js
@@ -0,0 +1,23 @@
+"use strict";
+
+window.onload = init();
+
+function init() 
+{
+	var regForm = document.getElementById("regform");
+	// var payment = document.getElementById("payment");
+
+    // document.getElementById("regform").onsubmit = function(){
+    //     myFunction();
+    // }
+
+    
+
+    function myFunction() {
+    alert("The form was submitted");
+}
+    
+	// regForm.onsubmit = validate();
+	// payment.onsubmit = validate_payment();
+    
+}
--- scripts/part2.js
+++ scripts/part2.js
@@ -1,8 +1,43 @@
 "use strict";
 
 //enquiry
-function validate(){
+var msg = "";
+var enquire = true;
+var payment = true;
 
+function init () 
+{   
+
+    if(!document.getElementById("regform")){
+        var payment = document.getElementById("payment");
+	    payment.submit = validate_payment();
+	    payment.submit = printdata();
+
+        	    
+    }
+    if(!document.getElementById("payment")){	
+	    var regForm = document.getElementById("regform");
+	    regForm.onsubmit = validate;
+
+        // if(enquire == true)
+        //     window.location = "payment.html";
+
+    }
+    
+}
+
+// if(document.getElementById("payment")){
+//     window.onload = function() {
+//      validate_payment();
+// };
+// }
+
+
+
+
+function validate(){
+    
+    // document.getElementById("error").innerHTML = "New text!";
 
     var total = 0;
     sessionStorage.fname = document.getElementById('fname').value;
@@ -13,6 +48,7 @@ function validate(){
     sessionStorage.town = document.getElementById('town').value;
     sessionStorage.state = document.getElementById('state').value;
     sessionStorage.postcode = document.getElementById('postcode').value;
+    
     if(document.getElementById('email').checked){
 
         sessionStorage.contact = document.getElementById('email').value;
@@ -25,64 +61,62 @@ function validate(){
     if(document.getElementById('cnt').checked){
         sessionStorage.contact = document.getElementById('cnt').value;     
     }
+    var product = document.getElementById("gift");
+    
+    //alert(product.value);
 
     sessionStorage.gift = document.getElementById('gift').value;
     
     sessionStorage.quantity = document.getElementById('quantity').value;
     var m = sessionStorage.gift.split("$");
-        // alert(sessionStorage.gift);
-        // alert(m[1]);
+        // msg += sessionStorage.gift);
+        // msg += m[1]);
 
     total = total + parseInt(parseInt(m[1])*parseInt(sessionStorage.quantity));
 
      if(document.getElementById('3b').checked){
         sessionStorage.b3 = document.getElementById('3b').value;
         var n = sessionStorage.b3.split("$");
-        // alert(n);
+        // msg += n);
         total = total + parseInt(n[1]);      
     }
     
     if(document.getElementById('2b').checked){
         sessionStorage.b2 = document.getElementById('2b').value;
         var m = sessionStorage.b2.split("$");
-        // alert(m[1]);
+        // msg += m[1]);
         total = total + parseInt(m[1]);     
     }
 
     sessionStorage.total = total;
 
-
-// alert(sessionStorage.fname);
-    quantity();
-    if (postcode() == false)
-        alert("Postcode invalid");
-}
-
-function quantity(){
-
+// msg += sessionStorage.fname);
+    // quantity();
     var product = document.getElementById("gift");
-    // alert(product.value);
+    // msg += product.value);
     
     if(product.value == "Choose"){
-        alert("Please Choose the product.");
-        return false;
+        msg += "Please Choose the product.<br>";
+        enquire = false;
     }
     
     else{
         var quantity = document.getElementById("quantity");
         var numbers = /^[0-9]+$/;
         if(!quantity.value.match(numbers) || quantity.value == 0) {  
-                alert('wrong quantity');  
-                return false;
+                msg += 'wrong quantity.<br>';  
+                enquire = false;
+                
         }
     }
-}
+    // if (postcode() == false)
+    //     msg += "Postcode invalid<br>";
 
-function postcode(){
-    var state = document.getElementById("state");
+      var state = document.getElementById("state");
     if(state.value == "none"){
-        alert("Please choose the state");
-        return false;
+        msg += "Please choose the state.<br>";
+        enquire = false;
+        
     }
     
     var postcode = document.getElementById("postcode");
@@ -132,7 +166,100 @@ function postcode(){
         } 
     }
 
-    return x;  
+    if(x == false){
+        enquire=false;  
+        msg += "Postcode invalid<br>";
+    }
+
+    // alert(msg);
+    document.getElementById("error").innerHTML = msg;
+
+
+    if(enquire == true)
+        window.location = "payment.html";
+    
+}
+
+function quantity(){
+
+    var product = document.getElementById("gift");
+    // msg += product.value);
+    
+    if(product.value == "Choose"){
+        msg += "Please Choose the product.<br>";
+        enquire = false;
+    }
+    
+    else{
+        var quantity = document.getElementById("quantity");
+        var numbers = /^[0-9]+$/;
+        if(!quantity.value.match(numbers) || quantity.value == 0) {  
+                msg += 'wrong quantity.<br>';  
+                enquire = false;
+                
+        }
+    }
+}
+
+function postcode(){
+    var state = document.getElementById("state");
+    if(state.value == "none"){
+        msg += "Please choose the state.<br>";
+        enquire = false;
+        
+    }
+    
+    var postcode = document.getElementById("postcode");
+    var first = postcode.value.charAt(0);
+    var x = false;
+    var numbers = /^[0-9]+$/;
+
+    if(postcode.value.match(numbers) && postcode.value.length == 4){
+        switch (state.value) {
+            case "VIC":
+                if(first==3 || first ==8)
+                    x=true;
+                break;
+            case "NSW":
+                if(first==1 || first ==2)
+                    x=true;
+                break;
+            
+            case "QLD":
+                if(first==4 || first ==9)
+                    x=true;
+                break;
+            case "NT":
+                if(first==0)
+                    x=true;
+                break;
+            case "WA":
+                if(first==6)
+                    x=true;
+                break;
+
+            case "SA":
+                if(first==5)
+                    x=true;
+                break;
+            case "TAS":
+                if(first==7)
+                    x=true;
+                break;
+            case "ACT":
+                if(first==0)
+                    x=true;
+                break;
+            default:
+                x = false;
+
+        } 
+    }
+
+    if(x == false)
+        enquire=false;  
+
+    
 }
 
 
@@ -142,10 +269,20 @@ function postcode(){
 function validate_payment(){
     
     card_name();
-    card_number();
     expire();
     check_card();
-    check_cvv();
+
+    // alert(msg);
+    
+    document.getElementById("cancelButton").addEventListener("click", myFunction);
+
+    function myFunction() {
+	    window.location = "index.html";
+    }
+  
+    
+
+    
 }
 
 
@@ -154,21 +291,23 @@ function card_name(){
     var name = /^[a-zA-Z ]*$/;
 
     if(!cardname.value.match(name) || cardname.value.length > 40){
-        alert("Enter valid card name");
+        msg += "Enter valid card name<br>";
         return false;
     }   
 }
 
 
 
-function card_number(){
-    var cardnumber = document.getElementById("cardnumber");
-    var numbers = /^[0-9]+$/;
-    if(!cardnumber.value.match(numbers) || !(cardnumber.value.length ==15 || cardnumber.value.length ==16) ){
-        alert("Enter valid card number");
-        return false;
-    }   
-}
+// function card_number(){
+//     var cardnumber = document.getElementById("cardnumber");
+//     var numbers = /^[0-9]+$/;
+//     var cardname = document.getElementById("cardname");
+    
+//     if(!cardnumber.value.match(numbers) || !(cardnumber.value.length ==15 || cardnumber.value.length ==16) ){
+//         msg += "Enter valid number<br>";
+//         return false;
+//     }   
+// }
 
 function expire(){
     var date = /^(0[1-9]|10|11|12)-[0-9]{2}$/;
@@ -177,7 +316,7 @@ function expire(){
 
 
     if(!expdate.value.match(date)){
-        alert("Enter valid expire date");
+        msg += "Enter valid expire date<br>";
         return false;
     }
 
@@ -190,7 +329,7 @@ function expire(){
         var year = parseInt(parts[1], 10);
         var month = parseInt(parts[0], 10);
         if (year < currentYear || (year == currentYear && month < currentMonth)) {
-            alert("The expiry date has passed");
+            msg += "The expiry date has passed<br>";
             return false;
         }
     }
@@ -210,14 +349,14 @@ function check_card(){
         if(cardnumber.value.match(numbers)){
 
             if((cvv.value.length != 3) || (!cvv.value.match(numbers))){
-                alert("Enter valid cvv number");
+                msg += "Enter valid cvv number<br>";
                 return false;
             
             }
 
         }
         else{
-            alert("Enter valid visa card number");
+            msg += "Enter valid visa card number<br>";
             return false;
             
         }
@@ -229,11 +368,11 @@ function check_card(){
         var number_old = /^[0-9]+$/;
         if(cardnumber.value.match(numbers)){
             if(cvv.value.length != 4 || !cvv.value.match(number_old)){
-                alert("Enter valid cvv number");
+                msg += "Enter valid cvv number<br>";
                 return false;
             }
         }else{
-            alert("Enter valid amex card number");
+            msg += "Enter valid amex card number<br>";
             return false;
         }   
         
@@ -245,11 +384,11 @@ function check_card(){
         var number_old = /^[0-9]+$/;
         if(cardnumber.value.match(numbers)){
            if(cvv.value.length != 3 || !cvv.value.match(number_old)){
-                alert("Enter valid cvv number");
+                msg += "Enter valid cvv number<br>";
                 return false;
             }
         }else{
-             alert("Enter valid mastercard number");
+             msg += "Enter valid mastercard number<br>";
              return false;
         }
         
@@ -261,22 +400,44 @@ function check_card(){
 
 
 function printdata(){
-    document.getElementById("fname").innerHTML = "First Name:" + sessionStorage.fname;
-    document.getElementById("lname").innerHTML = "Last Name:" + sessionStorage.lname;
-    document.getElementById("mail").innerHTML = "Email: " +sessionStorage.mail;
-    document.getElementById("phone").innerHTML = "Phone: " + sessionStorage.phone;
-    document.getElementById("street").innerHTML = "Street: " + sessionStorage.street;
-    document.getElementById("town").innerHTML = "town: " + sessionStorage.town;
-    document.getElementById("state").innerHTML = "state: " +sessionStorage.state;
-    document.getElementById("postcode").innerHTML = "postcode: " + sessionStorage.postcode;
-    document.getElementById("contact").innerHTML = "Prefered Contact: " +sessionStorage.contact;
-    document.getElementById("gift").innerHTML = "Item: " + sessionStorage.gift;
-    document.getElementById("quantity").innerHTML = "Quantity: " + sessionStorage.quantity;
+    document.getElementById("fname").innerHTML = sessionStorage.fname;
+    document.getElementById("lname").innerHTML = sessionStorage.lname;
+    document.getElementById("mail").innerHTML = sessionStorage.mail;
+    document.getElementById("phone").innerHTML = sessionStorage.phone;
+    document.getElementById("street").innerHTML = sessionStorage.street;
+    document.getElementById("town").innerHTML =  sessionStorage.town;
+    document.getElementById("state").innerHTML = sessionStorage.state;
+    document.getElementById("postcode").innerHTML =  sessionStorage.postcode;
+    document.getElementById("contact").innerHTML = sessionStorage.contact;
+    document.getElementById("gift").innerHTML = sessionStorage.gift;
+    document.getElementById("quantity").innerHTML = sessionStorage.quantity;
+    if(document.getElementById("2b").checked){
+        document.getElementById("2b").innerHTML = sessionStorage.b2;
+        
+    }
+
+    // if(document.getElementById("3b").checked){
+    //     document.getElementById("3b").innerHTML = sessionStorage.quantity;
+        
+    // }
     document.getElementById("total").innerHTML =  parseInt(sessionStorage.total);
-    
 }
 
 function logout(){
     localStorage.clear();
     window.location.href = "enquire.html";
 }
+
+
+
+// window.onload = printdata;
+
+window.onload = init;
+
+// var clickme = document.getElementById("cancelButton");
+// clickme.onclick = function(){
+    
+// 	window.location = "index.html";
+// }
+
+    
